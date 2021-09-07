b55f50672c2600aecf073937414ce0e037e28d0e
sourabhhsethii <sourabhhsethi8@linux.com>
2021-09-07 00:41:58 -0400

    findPrimeNumber approach added.

--- src/com/recursion/example/sep21/FindPrimeNumberTillNApproach4.java
+++ src/com/recursion/example/sep21/FindPrimeNumberTillNApproach4.java
@@ -10,20 +10,6 @@ Find the first number that is greater than p that is not marked. If there is no 
 When the algorithm terminates, the numbers that are not marked are all the prime numbers below n.
  */
 public class FindPrimeNumberTillNApproach4 {
-    public void findPrimeNumber(int n){ // O(n) * O(n) => O(n^2)
-        // Base Condition
-        if(n < 2){
-            return;
-        }
-
-        // Work;
-        if(primaryCheck(n) ){  //O(n)
-            System.out.print( n + " ");
-        }
-        findPrimeNumber(n-1); // fn(n-1) = n-1...n-2...n-3...2 => O(n)
-
-
-    }
 
 
     /**
@@ -32,22 +18,38 @@ public class FindPrimeNumberTillNApproach4 {
      * @param num
      * @return
      */
-    public boolean primaryCheck(int num){
-        if(num<1) return false;
-        if(num<=3) return true;
-        if (num%2 == 0 || num%3 ==0) return false;
-        for(int i=5; i<= Math.sqrt(num);i=i+6){
-            if(num%i ==0 || num%(i+2)==0){
-                return false;
+    public void primaryCheck(int num){
+
+        boolean[] marking = new boolean[num+1];
+
+        if(num<1) {
+            marking[0] = false;
+            marking[1] = false;
+        }
+        for(int i=2; i< num;i++){
+            marking[i] = true;
+            for (int j = i+1; j<num;j++){
+
+                    if(i%j == 0){
+                    marking[j] = false;
+                } else {
+                        marking[j] = true;
+                    }
+
             }
-            return true;
         }
 
-        return true;
+        for (int i=0;i<marking.length;i++){
+
+            if(marking[i]){
+                System.out.print(i + " ");
+            }
+
+        }
     }
 
     public static void main(String args[]){
         FindPrimeNumberTillNApproach4 fn = new FindPrimeNumberTillNApproach4();
-        fn.findPrimeNumber(33);
+        fn.primaryCheck(33);
     }
 }
