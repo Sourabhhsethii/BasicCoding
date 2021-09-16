5202c32d9468eeba2f427c2aebd96fee2f94a285
Sourabh Sethi <sourabhhsethii@linux.com>
2021-09-16 18:54:19 -0400

    Insertion Sort added.

---
+++ src/com/sorting/InsertionSort.java
@@ -0,0 +1,58 @@
+package com.sorting;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/*
+Insertion Sort
+
+Inputs
+6
+1
+7
+2
+6
+3
+5
+
+Output: [1, 2, 3, 5, 6, 7]
+ */
+public class InsertionSort {
+
+    public static void insertionSort(int[] arr){
+        int temp =0;
+        // iteration from second element in an array till last
+        for (int i=1;i<arr.length;i++){ // Correct
+            // Move towards left of selected i element
+
+            int v= arr[i];
+            int j=i;
+
+            while (j>=1 && arr[j-1]>v){
+
+                arr[j] = arr[j-1];
+                j--;
+
+            }
+            arr[j]=v;
+
+        }
+        System.out.println(Arrays.toString(arr));
+
+    }
+
+    public static void main(String[] args) {
+        int n;
+
+        System.out.println("Enter Number of elements of array");
+        Scanner input = new Scanner(System.in);
+        n = input.nextInt();
+
+        int[] num = new int[n];
+        for (int i = 0; i < n; i++){
+            num[i] = input.nextInt();
+        }
+        insertionSort(num);
+
+    }
+}
