30631651f3b35fe6fb3af8e82edceb2a96e5b4c4
Sourabh Sethi <sourabhhsethii@linux.com>
2021-10-11 14:02:07 -0400

    DFS Traversals. PRE-IN-POST.

--- src/com/binarytree/BinaryTreeRepresentation.java
+++ src/com/binarytree/BinaryTreeRepresentation.java
@@ -57,5 +57,55 @@ public class BinaryTreeRepresentation {
         tree.root.right.right = new Node(7);
 
         System.out.println(tree);
+
+        preOrderDFS(tree.root);
+        System.out.println(" ");
+        inOrderDFS(tree.root);
+        System.out.println(" ");
+        postOrderDFS(tree.root);
+    }
+    // Method to print the tree in pre-order traversal
+    static  void preOrderDFS(Node node) {
+        if (node == null)
+            return;
+
+        // visit the parent node (parent of left & right children)
+        System.out.print(node.data + " ");
+
+        // recursively go to left subtree
+        preOrderDFS(node.left);
+
+        // recursively go to right subtree
+        preOrderDFS(node.right);
+    }
+
+    // Method to print the tree in in-order traversal
+    static void inOrderDFS(Node node) {
+        if (node == null)
+            return;
+
+        // recursively go to left subtree
+        inOrderDFS(node.left);
+
+        // visit the parent node (parent of left & right children)
+        System.out.print(node.data + " ");
+
+        // recursively go to right subtree
+        inOrderDFS(node.right);
+    }
+
+    // Method to print the tree in post-order traversal
+    static void postOrderDFS(Node node) {
+        if (node == null)
+            return;
+
+        // recursively go to left subtree
+        postOrderDFS(node.left);
+
+        // recursively go to right subtree
+        postOrderDFS(node.right);
+
+        // visit the parent node (parent of left & right children)
+        System.out.print(node.data + " ");
     }
 }
