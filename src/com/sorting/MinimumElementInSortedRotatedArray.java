8fbebc05d3cb08383f6adff4f81ccbe269eb422a
Sourabh Sethi <sourabhhsethii@linux.com>
2021-10-16 23:02:42 -0400

    Minimum Element in a Sorted and Rotated Array

--- src/com/sorting/MinimumElementInSortedRotatedArray.java
+++ src/com/sorting/MinimumElementInSortedRotatedArray.java
@@ -63,7 +63,27 @@ public class MinimumElementInSortedRotatedArray {
 
     public static int minimum(int arr[], int low, int high)
     {
+        if(high<low){
+            return arr[0];
+        }
 
+        if(high == low){
+        return arr[low];
+        }
+        int mid = low +  (high - high)/2;
+
+        if(mid<high && arr[mid+1]<arr[mid]){
+            return arr[mid+1];
+        }
+
+        if(mid>low && arr[mid]<arr[mid-1]){
+            return arr[mid];
+        }
+
+        if(arr[high]> arr[mid]){
+            return minimum(arr,low,mid-1);
+        }
+        return minimum(arr,mid+1,high);
     }
 
     public static void main(String args[])
