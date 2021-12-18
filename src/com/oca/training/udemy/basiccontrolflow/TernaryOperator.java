1b6a9570f9e8cee306a69c775ae1d0394a95dc68
Sourabh Sethi <sourabhhsethii@linux.com>
2021-12-18 15:01:30 -0500

    basiccontrolflw

--- src/com/oca/training/udemy/basiccontrolflow/TernaryOperator.java
+++ src/com/oca/training/udemy/basiccontrolflow/TernaryOperator.java
@@ -20,8 +20,33 @@ public class TernaryOperator {
 
         System.out.println("y= " + y);
 
+        y = 1;
         // Not Same Datatype
         System.out.println(y > 5 ? 10 : "test");
       //  int myInt = y > 5 ? 10 : "test"; // this will not compile.
+
+        int a = 1;
+        int b = 1;
+
+        int c = a < 10 ? a++ : b++;
+        // c = a = 1;
+        // a = 2;
+        // b = 1 -> because it ++ is not executed because of short circut.
+        System.out.println(a + " , " + b + " , " + c ); // 2, 1, 1
+
+        int d = 1;
+        int e = 2;
+        int f = 3;
+
+        if(d<10){
+            f = d++ < 1? e++ : f++;
+            // d = 2;
+            // e = 2;
+            // f == 3;
+        } else {
+            f = (d+e) < 2 ? d++ : e++;
+        }
+
+        System.out.println(d+ ", "+ e + ", "+ f ); // 2,2,3
     }
 }
