859b9ea7d4d9df7065a6f6ea9a7ea4984d2093cb
Sourabh Sethi <sourabhhsethii@linux.com>
2021-12-18 14:46:41 -0500

    basiccontrolflw

---
+++ src/com/oca/training/udemy/udemybasiccontrolflw/IfElseStatement.java
@@ -0,0 +1,48 @@
+package com.oca.training.udemy.udemybasiccontrolflw;
+
+public class IfElseStatement {
+    public static void main(String[] args) {
+        boolean b = false;
+
+        if(!b){
+            System.out.println("true");
+        } else
+        {
+            System.out.println("false");
+        }
+
+        int x = 1;
+         // if(x){}
+        // if(x=1){}
+        if(x==1){}
+
+        int testScore = 90;
+        char grade = 0;
+        if(testScore>= 90){
+            grade = 'A';
+        } else if(testScore >= 80){
+            grade = 'B';
+        } else if (testScore >=70){
+            grade = 'c';
+        } else if(testScore >= 60){
+            grade = 'D';
+        } else  if(testScore >= 50){
+            grade = 'F';
+        }
+        System.out.println("grade = " + grade);
+
+        int hourOfDay = 10;
+        if(hourOfDay < 11) System.out.println("Morning");
+        else if (hourOfDay < 17) System.out.println("Evening");
+        else System.out.println("Day");
+
+        if(hourOfDay < 17){
+            System.out.println("Afternoon");
+        } else if(hourOfDay< 11) { // 11 is <17 this will never excute;
+            System.out.println("Morning");
+        } else
+        {
+            System.out.println("Evening");
+        }
+    }
+}
---
+++ src/com/oca/training/udemy/udemybasiccontrolflw/TernaryOperator.java
@@ -0,0 +1,23 @@
+package com.oca.training.udemy.udemybasiccontrolflw;
+
+public class TernaryOperator {
+
+    public static void main(String[] args) {
+        int x = 10;
+        int y;
+
+        if(x>5){
+            y = 2 * x;
+        } else {
+            y = 4 * x;
+        }
+
+        System.out.println("y= " + y);
+
+        // ternary operator
+       // y = x > 5 ? 2 * x : 4 * x;
+        y = (x>5)? (2 * x) : (4 *x); // nicer to read
+
+        System.out.println("y= " + y);
+    }
+}
