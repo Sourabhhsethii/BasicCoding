f312d66c357026b6154035db577bb5b62abce3d3
Sourabh Sethi <sourabhhsethii@linux.com>
2021-12-18 23:47:11 -0500

    Strings

---
+++ src/com/oca/training/udemy/strings/StringBuilderExample.java
@@ -0,0 +1,45 @@
+package com.oca.training.udemy.strings;
+
+public class StringBuilderExample {
+    public static void main(String[] args) {
+
+        StringBuilder mystring =  new StringBuilder();
+
+        for(char c ='a'; c<='z';c++){
+            mystring.append(c);
+        }
+        System.out.println(mystring);
+
+        // With String Builder
+        StringBuilder sb = new StringBuilder();
+        for(char c='a'; c<='z'; c++){
+            sb.append(c); // add char to string builder, reuses string builer without creating string
+        }
+
+        System.out.println(sb);
+
+        StringBuilder  builder = new StringBuilder("start");
+        builder.append("-middle"); // "start-middle"
+
+        StringBuilder anotherBuilder = builder.append("-end");
+
+        System.out.println();
+        System.out.println(builder);
+        System.out.println(anotherBuilder);
+
+        System.out.println(builder == anotherBuilder);
+        System.out.println(System.identityHashCode(builder));
+        System.out.println(System.identityHashCode(anotherBuilder));
+
+        System.out.println();
+
+        StringBuilder myBuilder =new StringBuilder();
+        System.out.println("size= " + myBuilder.length());
+        System.out.println("capacity= " + myBuilder.capacity());
+
+        myBuilder = new StringBuilder(100);
+        System.out.println("size= " + myBuilder.length());
+        System.out.println("capcity= " +myBuilder.capacity());
+
+    }
+}
