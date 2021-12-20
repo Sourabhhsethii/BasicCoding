a36b320c0c0ff6162547e87fa595a81f1c8046bc
Sourabh Sethi <sourabhhsethii@linux.com>
2021-12-20 15:01:02 -0500

    Dynamic Arrays

---
+++ src/com/oca/training/udemy/array/ArrayListExample.java
@@ -0,0 +1,27 @@
+package com.oca.training.udemy.array;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// Dynamic Array
+public class ArrayListExample {
+
+    public static void main(String[] args) {
+        ArrayList myList = new ArrayList();
+
+        ArrayList<Object> myList2 = new ArrayList<>(); // It can hold any things
+
+        ArrayList<String> list = new ArrayList<>(); // It can hold only Strings
+
+        ArrayList<String> list2 = new ArrayList<>();
+
+        List<String> list3 = new ArrayList<>(); // using interface as type // Poly-morphism
+
+        // ArrayList<String> list4 = new List<String>(); // Doesnt complie
+
+        List<String> list5 = new ArrayList<>(20);
+    }
+
+
+
+}
