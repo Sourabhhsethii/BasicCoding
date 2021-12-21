fa1afdacd0fb625911006cb81a2d2ad11323ca58
Sourabh Sethi <sourabhhsethii@linux.com>
2021-12-21 10:06:52 -0500

    Method Design

---
+++ src/com/oca/training/udemy/methods/MethodDesign.java
@@ -0,0 +1,57 @@
+package com.oca.training.udemy.methods;
+
+public class MethodDesign {
+    public static void main(String[] args) {
+
+    }
+
+    public void  jump(){
+
+    }
+
+    // Default access Modifier
+    void  jump3(){
+
+    }
+
+    //
+    public final void  jump4(){
+
+    }
+
+    public static final void jump5(String[] args) {
+
+    }
+
+    public final static  void jump6(String[] args) {
+
+    }
+
+    final public void jump8(){
+
+    }
+
+    final static public void jump9(){
+
+    }
+
+    // Two return type will not compile.
+    /*String int public void jump11(){
+
+    }*/
+
+
+    // Two return type will not compile.
+    /*public String void jump8(){
+
+    }*/
+
+    /*   public array void jump7(){
+
+    }*/
+
+    // Method will not complie
+   /* void public void jump2(){
+
+    }*/
+}
