6c7ba620a7471b502059c66cde1851272d2d6088
Sourabh Sethi <sourabhhsethii@linux.com>
2021-12-22 19:13:05 -0500

    Static Variables

---
+++ src/com/oca/training/udemy/methods/UsingStaticMethod2.java
@@ -0,0 +1,8 @@
+package com.oca.training.udemy.methods;
+
+public class UsingStaticMethod2 {
+    public static int count = 0;
+    public static void incrementCounter(){
+        count++;
+    }
+}
---
+++ src/com/oca/training/udemy/methods/UsingStaticMethodsPart1.java
@@ -0,0 +1,33 @@
+package com.oca.training.udemy.methods;
+
+public class UsingStaticMethodsPart1 {
+    public static void main(String[] args) {
+        UsingStaticMethod2 us = new UsingStaticMethod2();
+
+        System.out.println(us.count); // warning
+        System.out.println(UsingStaticMethod2.count);
+
+        UsingStaticMethod2 another = new UsingStaticMethod2();
+        another.incrementCounter(); // warning
+
+        UsingStaticMethod2.incrementCounter();
+        System.out.println(UsingStaticMethod2.count);
+
+        us = null;
+        another = null;
+
+        System.out.println(UsingStaticMethod2.count);
+        System.out.println(us.count);
+        System.out.println(another.count);
+
+        UsingStaticMethod2.count = 10;
+        UsingStaticMethod2 newInstance = new UsingStaticMethod2();
+        newInstance.count =5;
+        another.count =11;
+
+        System.out.println(UsingStaticMethod2.count); // 11 // beacuse static variable is shared with all the instances.
+        System.out.println(another.count);
+        System.out.println(us.count);
+
+    }
+}
