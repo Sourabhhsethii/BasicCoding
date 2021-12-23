5c8b53b6f8f94059c0e9368e2fc79bb5a34c3765
Sourabh Sethi <sourabhhsethii@linux.com>
2021-12-22 19:00:07 -0500

    Static Methods

--- src/com/oca/training/udemy/common/AnotherCommon.java
+++ src/com/oca/training/udemy/common/AnotherCommon.java
@@ -1,5 +1,7 @@
 package com.oca.training.udemy.common;
 
+import com.oca.training.udemy.methods.StaticMethodAndFields;
+
 public class AnotherCommon {
 
     public static void main(String[] args) {
@@ -13,5 +15,10 @@ public class AnotherCommon {
         System.out.println("protectedNumber= " +  common.protectedNumber);
         System.out.println("defaultInt= " +  common.defaultInt);
         // System.out.println("privateInt= " +  common.privateInt); // private access
+
+        StaticMethodAndFields.main(args);
+        System.out.println(StaticMethodAndFields.myNumber);
+
+       // Common.main(args);
     }
 }
---
+++ src/com/oca/training/udemy/methods/StaticMethodAndFields.java
@@ -0,0 +1,11 @@
+package com.oca.training.udemy.methods;
+
+public class StaticMethodAndFields {
+
+    public static int myNumber = 10;
+    public static void main(String[] args) {
+        StaticMethodAndFields  s =  new StaticMethodAndFields();
+
+        System.out.println(myNumber);
+    }
+}
