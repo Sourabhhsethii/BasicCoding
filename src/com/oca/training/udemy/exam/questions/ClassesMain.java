c9e7bbbcd73b0ce350c1e48068ae48b939ebe33a
Sourabh Sethi <sourabhhsethii@linux.com>
2021-12-28 14:13:16 -0500

    Time.

--- src/com/oca/training/udemy/exam/questions/ClassesMain.java
+++ src/com/oca/training/udemy/exam/questions/ClassesMain.java
@@ -12,10 +12,15 @@ public  class ClassesMain {
        // LocalDate date = LocalDate.of(2020, Month.APRIL,40); // Runtime Error
        // System.out.println(date.getYear() + " " + date.getDayOfMonth() + " " + date.getDayOfMonth() + " " + date.getMonth());
 
-        LocalDateTime d = LocalDateTime.of(2015,5,10,11,22,33);
+     /*   LocalDateTime d = LocalDateTime.of(2015,5,10,11,22,33);
         Period p = Period.of(1,2,3);
         d = d.minus(p);
         DateTimeFormatter f = DateTimeFormatter.ofLocalizedTime(FormatStyle.SHORT);
+        System.out.println(d.format(f));*/
+        LocalDateTime d = LocalDateTime.of(2017,5,10,11,22,33);
+        Period p = Period.ofDays(1).ofYears(2);
+        d = d.minus(p);
+        DateTimeFormatter f = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.SHORT);
         System.out.println(d.format(f));
 
     }
