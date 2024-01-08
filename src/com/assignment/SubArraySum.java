4c05882b69060754f9fb8818d086ff7ba7739998
Sourabh Sethi <sourabhhsethii@linux.com>
2024-01-08 01:49:44 -0500

    added sum of all SubArraySum with O(N^3) & O(N^2) & O(N)

--- src/com/assignment/SubArraySum.java
+++ src/com/assignment/SubArraySum.java
@@ -7,7 +7,7 @@ public class SubArraySum {
         A[0] = 1;
         A[1] = 2;
         A[2] = 3;
-        System.out.println(subarraySum2(A));
+        System.out.println(subarraySum3(A));
     }
 
     // O(N*N*N) => N^3 - Complexity!!!
@@ -30,11 +30,28 @@ public class SubArraySum {
     public static long subarraySum2(int[] A) {
         long arraySum =0;
         for(int i=0;i<A.length;i++) {
-                int sum = 0;
+                long sum = 0;
             for(int j=i; j<A.length;j++){
-              sum += A[j];
+              sum =  sum  + A[j];
+              arraySum = arraySum  + sum;
             }
-            System.out.println(sum);
+        }
+        return arraySum;
+    }
+
+    /**
+     * Contribution technique - Contribution of every element;
+     * @param A
+     * @return
+     */
+    // O(N*N) => N^2 - Complexity!!!
+    public static long subarraySum3(int[] A) {
+        long arraySum =0;
+        for(int i=0;i<A.length;i++) {
+           long left = i+1;
+           long right = A.length-i;
+           long total = left * right;
+           arraySum = arraySum + (total * A[i]);
         }
         return arraySum;
     }
