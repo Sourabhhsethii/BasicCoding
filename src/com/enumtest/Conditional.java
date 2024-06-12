57f3a30ed36fe7060078e673970648f90c2e7d0b
Sourabh Sethi <sourabhhsethii@linux.com>
2024-06-12 17:38:11 -0400

    ENUM added.

---
+++ src/com/enumtest/Conditional.java
@@ -0,0 +1,11 @@
+package com.enumtest;
+
+enum Conditional {
+    TRUE,
+    UNKNOWN,
+    FALSE;
+
+    Conditional(){
+        System.out.println("Constructor Called");
+    }
+}
---
+++ src/com/enumtest/Test.java
@@ -0,0 +1,7 @@
+package com.enumtest;
+
+public class Test {
+    public static void main(String[] args) {
+        Conditional conditional = Conditional.FALSE;
+    }
+}
