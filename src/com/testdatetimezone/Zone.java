db3538978a95671abbef63073daec9a25e2e153c
Sourabh Sethi <sourabhhsethii@linux.com>
2024-06-23 03:46:09 -0400

    Added Period & DUration

--- src/com/testdatetimezone/Zone.java
+++ src/com/testdatetimezone/Zone.java
@@ -1,5 +1,7 @@
 package com.testdatetimezone;
 
+import java.time.Duration;
+import java.time.Period;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
@@ -11,5 +13,19 @@ public class Zone {
         System.out.println(zonedDateTime);
         System.out.println(zonedDateTime.toInstant());
         System.out.println(zonedDateTime.format(DateTimeFormatter.ISO_DATE));
+
+        Period period = Period.ofMonths(1);
+        System.out.println(zonedDateTime.plus(period));
+
+        Duration duration = Duration.ofHours(10);
+
+        int i = 0;
+        while (i<30)
+        {
+            zonedDateTime = zonedDateTime.plus(period);
+            zonedDateTime = zonedDateTime.minus(duration);
+            System.out.println(zonedDateTime);
+        i++;
+        }
     }
 }
