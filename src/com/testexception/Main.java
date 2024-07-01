ee5483d87d1ffb89dd6be2e897daa3c3d01a0ca8
Sourabh Sethi <sourabhhsethii@linux.com>
2024-07-01 14:20:26 -0400

    effectivelyFinal added.

--- src/com/testexception/Main.java
+++ src/com/testexception/Main.java
@@ -3,9 +3,9 @@ package com.testexception;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println(" Exception Examples ");
+        System.out.println("Exception Examples ");
         var effectivelyFinal  = new EffectivelyFinal();
-        // effectivelyFinal is effectively final.
+        // effectivelyFinal is effectively final.\
         try {
             new Exception();
         } catch (Exception exception){
@@ -18,22 +18,22 @@ public class Main {
             System.out.println(e);
         }
 
-        try(effectivelyFinal;var test = new MyTryException();
-            var test2 = new MyTryException2();) {
+        try(effectivelyFinal;
+            var test = new MyTryException();
+            var test2 = new MyTryException2(); ) {
             System.out.println("Started Try Block");
 
             System.out.println("Closing Try Block");
             throw new NullPointerException();
             // UnReachable Block after this...
         } catch (Exception e) {
-
             for (var th : e.getSuppressed())
                 System.out.println(th.getMessage());
             e.printStackTrace();
         } finally {
             System.out.println("Tested Completed!!");
         }
-
+        // uncomment below line; it will show you the example about what EffectivelyFinal
         // effectivelyFinal = null;
 
     }
